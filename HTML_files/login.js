4b954ae340bc0d1c32622603ac2f21372ba81874
GavinSpens <gavinspens@gmail.com>
2024-02-24 16:01:29 -0700

    modified:   HTML_files/Login.html 	modified:   HTML_files/forgot.css 	modified:   HTML_files/login.css 	modified:   HTML_files/login.js 	modified:   HTML_files/main.css 	modified:   HTML_files/profile.css 	modified:   HTML_files/register.css 	modified:   HTML_files/upload.css 	modified:   HTML_files/verified.css 	modified:   HTML_files/video.css

--- HTML_files/login.js
+++ HTML_files/login.js
@@ -1,12 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    //for testing only
+    localStorage.setItem('email-address', 'test');
+    localStorage.setItem('password', '1234');
+
     const emailInput = document.querySelector('.email-input');
+    const emailAddress = localStorage.getItem('email-address');
     const passwordInput = document.querySelector('.password-input');
+    const password = localStorage.getItem('password');
     const submitBtn = document.querySelector('.btn.btn-primary');
+    const incorrect = document.querySelector('.incorrect');
 
     submitBtn.addEventListener('click', () => {
-        const emailValidation = new RegExp(".*@.*\..*")
-        if (emailValidation.test(emailInput.value)) {
-            
+        if (password === passwordInput.value && emailAddress === emailInput.value) {
+            localStorage.setItem('loggedIn', true);
+            window.location.href = 'index.html';
+        } else {
+            incorrect.classList.add('visible');
         }
     });
 });
