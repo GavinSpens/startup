7fdae354f058d955019a451c4da69dfeab21c034
GavinSpens <gavinspens@gmail.com>
2024-02-24 23:47:26 -0700

    modified:   HTML_files/main.js

--- HTML_files/main.js
+++ HTML_files/main.js
@@ -1,16 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const loggedIn = localStorage.getItem('loggedIn');
-    const logInOut_btn1 = document.querySelector("button.btn.btn-primary[type='login']");
-    const logInOut_btn2 = document.querySelector("a.btn.btn-primary[type='login']");
+    const logIn_btn1 = document.querySelector("button.btn.btn-primary[type='login']");
+    const logIn_btn2 = document.querySelector("a.btn.btn-primary[type='login']");
     const navbar = document.querySelector(".container-fluid.navbar.fixed-top.navbar-dark");
     const wrapper = document.querySelector(".wrapper.login-button");
     const emailAddress = localStorage.getItem('email-address');
     const footerDiv = document.querySelector("footer div");
+    let loggedIn = JSON.parse(localStorage.getItem('loggedIn'));
 
     if (loggedIn) {
         navbar.innerHTML = `<a class="navbar-brand" href="#"><img src="play.png" alt="Logo" height="50px" width="50px">Video Library</a>${emailAddress}<input type="text" class="Search" name="varSearch" placeholder="Search..."><button class="btn btn-primary" type="login" value="Login">Log Out</button>`;
         wrapper.innerHTML = '<a class="btn btn-primary" role="button" type="login">Log Out</a>';
         footerDiv.innerHTML = `<span class="text-reset">${emailAddress}</span>`
+        
+        const logOut_btn1 = document.querySelector("button.btn.btn-primary[type='login']");
+        const logOut_btn2 = document.querySelector("a.btn.btn-primary[type='login']");
+    
+        logOut_btn1.addEventListener('click', logOut);
+        logOut_btn2.addEventListener('click', logOut);
     }
 
     function logOut() {
@@ -21,7 +27,8 @@ document.addEventListener('DOMContentLoaded', () => {
         window.location.href = 'index.html';
     }
 
-    logInOut_btn1.addEventListener('click', () => {
+    logIn_btn1.addEventListener('click', () => {
+        loggedIn = JSON.parse(localStorage.getItem('loggedIn'));
         if (loggedIn) {
             logOut();
         } else {
@@ -29,7 +36,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    logInOut_btn2.addEventListener('click', () => {
+    logIn_btn2.addEventListener('click', () => {
+        loggedIn = JSON.parse(localStorage.getItem('loggedIn'));
         if (loggedIn) {
             logOut();
         } else {
