cc2047d463ffc5db8aa1b13565a891c654f43666
GavinSpens <gavinspens@gmail.com>
2024-03-02 00:09:08 -0700

    modified:   HTML_files/profile.js

--- HTML_files/profile.js
+++ HTML_files/profile.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    editBtn = document.getElementById('.edit-btn');
+    const editBtn = document.getElementById('.edit-btn');
+    const verified = JSON.parse(localStorage.getItem('verified'));
+
+    if (verified) {
+        makeVerified();
+    }
+
+    function makeVerified() {
+        //we'll see?
+    }
 
     editBtn.addEventListener('click', () => {
         //idk if I'll implement the profile page actually
