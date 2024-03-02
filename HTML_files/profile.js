3af047c0ecb360957b215db3bf9f296464547a08
GavinSpens <gavinspens@gmail.com>
2024-03-02 00:03:06 -0700

    modified:   HTML_files/profile.css 	modified:   HTML_files/profile.html 	new file:   HTML_files/profile.js

---
+++ HTML_files/profile.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    editBtn = document.getElementById('.edit-btn');
+
+    editBtn.addEventListener('click', () => {
+        //idk if I'll implement the profile page actually
+    });
+});
