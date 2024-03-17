ad1252fde9a345dcdf544f3b937a38913d92e021
GavinSpens <gavinspens@gmail.com>
2024-03-16 23:52:11 -0600

    modified:   Startup/Public/all.js 	modified:   Startup/Public/login.html 	modified:   Startup/Public/login.js

--- Startup/Public/all.js
+++ Startup/Public/all.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const db_js = require('../database.js');
     const LoginButton = document.getElementById('Login');
     const ProfileButton = document.getElementById('Profile');
-
-    let userName = '';
-    let password = '';
-    let hostname = '';    
+    const SubmitButton = document.getElementById('SubmitLogin');
+    const emailInput = document.getElementById('email-input');
+    const passwordInput = document.getElementById('password-input');
     
+
+    //PAGE NAVIGATION//
     LoginButton.addEventListener('click', () => {
         location.window.href = '/login';
     });
@@ -14,17 +16,23 @@ document.addEventListener('DOMContentLoaded', function() {
         location.window.href = '/profile';
     });
 
-    function Mongo (f) {
-        const { MongoClient } = require('mongodb');
-        const url = `mongodb+srv://${userName}:${password}@${hostname}`;
-        const client = new MongoClient(url);
-        client.connect(err => {
-            const collection = client.db("test").collection("devices");
 
-            // ... perform actions on the DB collection
-            f();
+    //REGISTER//
+    
 
-            client.close();
-        });
+
+    //LOGIN AND LOGOUT//
+    async function Login(email, password) {
+        const user = await db_js.getUser(email);
+        if (user) {
+            if (bcrypt.compare(password, user.password)) {
+                return user;
+            }
+        }
+        return null;
     }
+
+    SubmitButton.addEventListener('click', () => {
+        Login(emailInput.value, passwordInput.value);
+    });
 });
--- Startup/Public/login.js
+++ Startup/Public/login.js
@@ -1,18 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const emailInput = document.getElementById('email-input');
-    const passwordInput = document.getElementById('password-input');
-    const SubmitButton = document.getElementById('Submit');
     const RegisterButton = document.getElementById('Register');
-    let emailAddress = '';
-    let password = '';
 
     RegisterButton.addEventListener('click', () => {
         window.location.href = 'register.html';
-    });
-
-    SubmitButton.addEventListener('click', () => {
-        emailAddress = emailInput.value;
-        password = passwordInput.value;
-        
     });
 });
